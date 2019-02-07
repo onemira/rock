d4ee7bd4a4997a41a4e9d88dd953905785ab6079
Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux <mirabulgogi@gmail.com>
2019-02-07 09:38:10 -0500

    first commit

--- public/main.js
+++ public/main.js
@@ -1,7 +1,59 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+let suits = ['diamonds', 'hearts', 'spades', 'clubs']
+let deck = []
+
+const buildDeck = () => {
+	deck = []
+
+	for (let i = 0; i < suits.length; i++) {
+		for (let x = 0; x < cards.length; x++) {
+			var card = {
+				Value: cards[x],
+				Suit: suits[i]
+			};
+			deck.push(card);
+		}
+	}
+	return deck
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const shuffle = () => {
+	for (let i = 0; i < 1000; i++) {
+		let location1 = Math.floor(Math.random() * deck.length)
+		let location2 = Math.floor(Math.random() * deck.length)
+		let temp = deck[location1]
+		deck[location1] = deck[location2]
+		deck[location2] = temp
+	}
+
+	dealCards()
+}
+
+const dealCards = () => {
+	document.getElementById('deck').innerHTML = ''
+	for (let i = 0; i < deck.length; i++) {
+		let card = document.createElement('div')
+		let icon = ''
+		if (deck[i].Suit === 'hearts') icon = '&hearts;'
+		else if (deck[i].suit === 'spades') {
+			icon = '&spades;'
+		} else if (deck[i].suit === 'diamonds') {
+			icon = '&diams;'
+		} else {
+			icon = '&clubs;'
+		}
+
+		card.innerHTML = deck[i].Value + '' + icon
+		card.className = 'card'
+
+		document.getElementById('deck').appendChild(card)
+	}
+}
+
+const load = () => {
+	deck = buildDeck()
+	shuffle()
+	dealCards()
+}
+
+window.onload = load
